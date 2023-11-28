e6954647a2f6601fd4f3c18b163fd49152c85ecf
pchuan98 <haeer98@outlook.com>
2023-11-28 17:29:47 +0800

    refactor: remove collection

--- src/Lift.UI.Shared/Controls/Transfer/Transfer.cs
+++ src/Lift.UI.Shared/Controls/Transfer/Transfer.cs
@@ -1,12 +1,14 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Input;
-using Lift.UI.Collections;
 using Lift.UI.Data;
 using Lift.UI.Interactivity;
 
@@ -164,5 +166,84 @@ public class Transfer : ListBox
         }
 
         oldSelectedItems.CanNotify = true;
+    }
+}
+
+
+
+[Serializable]
+public class ManualObservableCollection<T> : ObservableCollection<T>
+{
+    private const string CountString = "Count";
+
+    private const string IndexerName = "Item[]";
+
+    private int _oldCount;
+
+    private bool _canNotify = true;
+
+    public bool CanNotify
+    {
+        get => _canNotify;
+        set
+        {
+            _canNotify = value;
+
+            if (value)
+            {
+                if (_oldCount != Count)
+                {
+                    OnPropertyChanged(new PropertyChangedEventArgs(CountString));
+                }
+
+                OnPropertyChanged(new PropertyChangedEventArgs(IndexerName));
+                OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+
+            }
+            else
+            {
+                _oldCount = Count;
+            }
+        }
+    }
+
+    public ManualObservableCollection()
+    {
+
+    }
+
+    public ManualObservableCollection(List<T> list) : base(list != null ? new List<T>(list.Count) : list) => CopyFrom(list);
+
+    public ManualObservableCollection(IEnumerable<T> collection)
+    {
+        if (collection == null) throw new ArgumentNullException(nameof(collection));
+        CopyFrom(collection);
+    }
+
+    protected override void OnPropertyChanged(PropertyChangedEventArgs e)
+    {
+        if (!CanNotify) return;
+
+        base.OnPropertyChanged(e);
+    }
+
+    protected override void OnCollectionChanged(NotifyCollectionChangedEventArgs e)
+    {
+        if (!CanNotify) return;
+
+        base.OnCollectionChanged(e);
+    }
+
+    private void CopyFrom(IEnumerable<T> collection)
+    {
+        var items = Items;
+        if (collection != null)
+        {
+            using var enumerator = collection.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                items.Add(enumerator.Current);
+            }
+        }
     }
 }

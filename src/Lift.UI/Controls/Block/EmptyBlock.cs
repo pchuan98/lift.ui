c10dbcab0ccf7d07ba1a5a4018ba6fe737c649c2
pchuan98 <haeer98@outlook.com>
2024-08-09 09:49:22 +0800

    feat: add EmptyBlock and ToggleBlock controls
    1. The EmptyBlock only used as element placeholder
    2. The ToggleBlock is more like ToggleButton, but it can carry changeds in elements.

---
+++ src/Lift.UI/Controls/Block/EmptyBlock.cs
@@ -0,0 +1,8 @@
+﻿using System.Windows.Controls;
+
+namespace Lift.UI.Controls.Block;
+
+/// <summary>
+/// 空占位符
+/// </summary>
+public class EmptyBlock : ContentControl;
---
+++ src/Lift.UI/Controls/Block/ToggleBlock.cs
@@ -0,0 +1,39 @@
+﻿// reference: https://github.com/HandyOrg/HandyControl/blob/master/src/Shared/HandyControl_Shared/Controls/Block/ToggleBlock.cs
+
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using DependencyPropertyGenerator;
+using Lift.UI.Commands;
+
+namespace Lift.UI.Controls.Block;
+
+/// <summary>
+/// 用来切换展示两个元素的占位符
+/// </summary>
+[DependencyProperty<bool?>("IsChecked", DefaultValueExpression = "false", Journal = true, TypeConverter = typeof(NullableBoolConverter))]
+[DependencyProperty<object>("CheckedContent")]
+[DependencyProperty<object>("UnCheckedContent")]
+[DependencyProperty<object>("NullContent")]
+public partial class ToggleBlock : Control
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public ToggleBlock()
+    {
+        CommandBindings.Add(new CommandBinding(ControlCommands.Toggle,
+            (_, _) => SetCurrentValue(IsCheckedProperty, IsChecked != true)));
+    }
+
+    /// <summary>
+    /// <inheritdoc />
+    /// </summary>
+    /// <param name="e"></param>
+    protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+    {
+        base.OnMouseLeftButtonDown(e);
+
+        SetCurrentValue(IsCheckedProperty, !IsChecked);
+    }
+}

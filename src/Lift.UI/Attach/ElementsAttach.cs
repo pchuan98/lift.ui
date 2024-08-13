c96f442f46cd1ec145b98b52af4b629bc42a4a23
pchuan98 <haeer98@outlook.com>
2024-08-13 14:03:09 +0800

    feat: add some temp value attach props

---
+++ src/Lift.UI/Attach/ElementsAttach.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using DependencyPropertyGenerator;
+
+namespace Lift.UI.Attach;
+
+/// <summary>
+/// 
+/// </summary>
+[AttachedDependencyProperty<int>("Int32", DefaultValue = 0, Inherits = true)]
+[AttachedDependencyProperty<double>("Double", DefaultValue = 0, Inherits = true)]
+public partial class Element;
+
+/// <summary>
+/// 
+/// </summary>
+[AttachedDependencyProperty<CornerRadius>("CornerRadius", Inherits = true)]
+public partial class BorderElement;
+
+

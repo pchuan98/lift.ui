8451c5e0256e4b8da61ddf8a51145685b655e305
pchuan98 <haeer98@outlook.com>
2024-08-13 14:05:33 +0800

    feat: add own ui library commands

---
+++ src/Lift.UI/Commands/ControlCommands.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Input;
+
+namespace Lift.UI.Commands;
+
+/// <summary>
+/// All control commands in this lib
+/// </summary>
+public static class ControlCommands
+{
+    /// <summary>
+    /// 切换任意两个有且仅有两个可切换的对象
+    /// </summary>
+    public static RoutedCommand Toggle { get; } = new(nameof(Toggle), typeof(ControlCommands));
+
+}
